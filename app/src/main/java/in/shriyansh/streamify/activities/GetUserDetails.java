c3b7843fbd6cc42d1aa1b0a2309b547617b5a93c
YashJipkate <yashjipkate@gmail.com>
2018-07-07 03:32:43 +0530

    Updated GetUserDetails xml

--- app/src/main/java/in/shriyansh/streamify/activities/GetUserDetails.java
+++ app/src/main/java/in/shriyansh/streamify/activities/GetUserDetails.java
@@ -1,6 +1,9 @@
 package in.shriyansh.streamify.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,11 +13,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
 import in.shriyansh.streamify.utils.PreferenceUtils;
 
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Objects;
 
 import in.shriyansh.streamify.R;
@@ -23,6 +31,7 @@ public class GetUserDetails extends AppCompatActivity implements OnItemSelectedL
 
     private String branch;
     private String year_join;
+    public static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +41,15 @@ public class GetUserDetails extends AppCompatActivity implements OnItemSelectedL
         setSupportActionBar(toolbar);
 
         Spinner branch_spinner = (Spinner) findViewById(R.id.branch_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> branch_adapter = ArrayAdapter.createFromResource(this,
                 R.array.branch_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         branch_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         branch_spinner.setAdapter(branch_adapter);
 
         Spinner year_spinner = (Spinner) findViewById(R.id.year_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> year_adapter = ArrayAdapter.createFromResource(this,
                 R.array.year_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         year_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         year_spinner.setAdapter(year_adapter);
 
         Button submit_button = (Button) findViewById(R.id.btn_submit);
@@ -59,6 +62,47 @@ public class GetUserDetails extends AppCompatActivity implements OnItemSelectedL
                 finish();
             }
         });
+
+        Button upload_btn = findViewById(R.id.pick_photo);
+        upload_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+                startActivityForResult(chooserIntent, PICK_IMAGE);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        ImageView user_img = findViewById(R.id.user_photo);
+
+        if (requestCode == PICK_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    user_img.setImageBitmap(selectedImage);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(GetUserDetails.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+
+            }else {
+                Toast.makeText(GetUserDetails.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
