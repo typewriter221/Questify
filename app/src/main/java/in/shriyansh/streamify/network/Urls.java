666df7c4cce6fe65c8e0a3c1963bd2a6a0f87efd
Akshay Sharma <contact@imakshay.com>
2018-08-29 13:13:21 +0530

    update URLs.java

--- app/src/main/java/in/shriyansh/streamify/network/Urls.java
+++ app/src/main/java/in/shriyansh/streamify/network/Urls.java
@@ -4,13 +4,13 @@ package in.shriyansh.streamify.network;
  * Created by shriyansh on 17/8/15.
  */
 public interface Urls {
-    String HOST = "https://sntc-streamify.herokuapp.com//";
+    String HOST = "https://sntc-streamify.herokuapp.com/";
     String DROPBOX_URL = "www.dropbox.com";
     String DROPBOX_CONTENT_URL = "dl.dropboxusercontent.com";
     String YOUTUBE_SLATE_URL = "https://img.youtube.com";
 
     String REGISTER_URL = HOST + "app/register";
-    String FCM_UPDATE = HOST + "fcm_update";
+    String FCM_UPDATE = HOST + "app/updateFCMToken";
     String GET_STREAMS = HOST + "app/get_streams";
     String GET_NOTIFICATIONS = HOST + "app/get_notifications";
     String GET_EVENTS = HOST + "app/get_events";
